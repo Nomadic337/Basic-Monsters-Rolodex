026a72df096f47d50d4c2a82880bb1718f163d0f
Wolf S Laleau <devlaleau@gmail.com>
2020-07-17 19:17:06 -0400

    Updates

---
+++ precache-manifest.3f9b4da420537b1ac190c3d89539a2f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06161d78a3dcb83dc173b08a8169b327",
+    "url": "/basic-monster-rolodex/index.html"
+  },
+  {
+    "revision": "d7c25df387a0a6520dee",
+    "url": "/basic-monster-rolodex/static/css/main.6837cd53.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/basic-monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basic-monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7c25df387a0a6520dee",
+    "url": "/basic-monster-rolodex/static/js/main.9aac7533.chunk.js"
+  },
+  {
+    "revision": "0a5ce0a2e743dfe2ef64",
+    "url": "/basic-monster-rolodex/static/js/runtime-main.2b951d16.js"
+  }
+]);
